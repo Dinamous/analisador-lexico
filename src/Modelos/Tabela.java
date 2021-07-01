26821209630d00d1062c9deb993d21f82c2b2086
matheus simoes <matheusaugustosimoes@hotmail.com>
2021-07-01 19:24:45 -0300

    Esqueci de colocar o Arraylist de Tabela

--- src/Modelos/Tabela.java
+++ src/Modelos/Tabela.java
@@ -5,38 +5,22 @@
  */
 package Modelos;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author matheus
  */
 public class Tabela {
     
-    public int id;
-    public String lexema;
-    public String token;
-    public float valor_inicial;
-    public String escopo;
-    public int lin;
-    public int col;
-    public int linha;
-    
-
+    ArrayList<Celula> celulas ;
+   
     public Tabela() {
+        
+       
     }
 
-    public Tabela( String lexema, String token, float valor_inicial, String escopo, int lin, int col, int linha) {
-        this.lexema = lexema;
-        this.token = token;
-        this.valor_inicial = valor_inicial;
-        this.escopo = escopo;
-        this.lin = lin;
-        this.col = col;
-        this.linha = linha;
-    }
-
-    public void IncrementaId() {
-        this.id = id++;
-    }
+   
     
     
     
